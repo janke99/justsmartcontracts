953e2dfb3e54e26ba6be47f8296af6a688136444
johhonn <35551051+johhonn@users.noreply.github.com>
2020-10-14 21:38:39 -0400

    Update ContractOperationsView.jsx
    Fix operations display

--- src/components/contract/ContractOperationsView.jsx
+++ src/components/contract/ContractOperationsView.jsx
@@ -87,7 +87,7 @@ class ContractOperationsView extends React.Component {
     render() {
         //select only non-constant methods
         let methods = this.props.contract._jsonInterface.filter(
-            item => item.constant === false
+            item =>((item.stateMutability !== "view")&&(item.stateMutability))
         );
 
         return (
